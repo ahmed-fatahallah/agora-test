230a4a80ac7040b7c992ea067ef14b892f9b8ec1
Ahmed_Saeed <ahmed.fathalla@nagwa.com>
2023-09-27 15:15:44 +0300

    chore: Change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "agora-rtc-sdk-ng": resolve(__dirname, "node_modules/agora-rtc-sdk-ng"),
+    },
+  },
   server: {
     host: true,
     port: 5000,
